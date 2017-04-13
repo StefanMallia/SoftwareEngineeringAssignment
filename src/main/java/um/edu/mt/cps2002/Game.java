7616b2ed542ac4945a30bd6c3d1d598fc36814e0
tmuscat95 <timothy.muscat.13@um.edu.mt>
2017-04-13 21:27:27 +0200

    Implemented Game and Map classes

--- src/main/java/um/edu/mt/cps2002/Game.java
+++ src/main/java/um/edu/mt/cps2002/Game.java
@@ -1,28 +1,42 @@
 package um.edu.mt.cps2002;
 
+
 import java.io.*;
+import java.util.Scanner;
 
 public class Game
 {
-    int turn;
-    Player[] players;
-    Map map;
+    static int turn;
+    static Player[] players;
+    static Map map;
 
-    public boolean setNumPlayers(int n)
-    {
-        return true;
-    }
-    public void startGame()
-    {
+    public static boolean setNumPlayers(int n) {
+        if(n>8 || n<2) return false;
+        else {
+            players = new Player[n];
+            return true;
+            }
+        }
 
-    }
-    public void generateHTMLFiles(String directory)
-    {
-
+    public void startGame() {
 
     }
-    public static void main(String[] args)
+
+    public static void generateHTMLFiles(String directory)
     {
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter Number of players \n");
+        java.util.Scanner s = new Scanner(System.in);
+
+        while (!setNumPlayers(s.nextInt()))
+            System.out.println("Please enter number between 2 and 8 inclusive");
+
+        map = new Map();
+        map.generate();
+
 
     }
 }
--- src/main/java/um/edu/mt/cps2002/Map.java
+++ src/main/java/um/edu/mt/cps2002/Map.java
@@ -4,17 +4,55 @@ public class Map
 {
     int size;
     Colour[][] tileColours;
+
     public boolean setMapSize(int size, int players)
     {
-        return true;
-    }
-    public void generate()
-    {
+        if(size<5) return false;
+        else if(size>50) return false;
+        else if(size<8 && players>4) return false;
+        else{
+            this.size = size;
+            tileColours = new Colour[size][size];
+            return true;
+        }
+
 
     }
+
+    public void generate()
+    {
+        int r;
+
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                r = ((int) (Math.random()%2)+1);
+
+                switch(r){
+                    case 1:
+                        tileColours[i][j] = Colour.GREEN;
+                        break;
+                    default:
+                        tileColours[i][j] = Colour.BLUE;
+
+
+                }
+            }
+
+
+        }
+
+        int I =  (int) Math.random()%size;
+        int J = (int) Math.random()%size;
+
+        tileColours[I][J] = Colour.YELLOW;
+
+
+    }
+
     public Colour getTileType(int row, int column)
     {
-        return Colour.GREEN;
+
+        return  tileColours[row][column];
     }
 
 }
