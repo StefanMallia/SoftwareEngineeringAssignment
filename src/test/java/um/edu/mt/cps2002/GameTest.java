5b90c4906289be6cfc0ca973469867d9b418d496
tmuscat95 <timothy.muscat.13@um.edu.mt>
2017-04-14 15:26:23 +0200

    Modified Game and Player tests

--- src/test/java/um/edu/mt/cps2002/GameTest.java
+++ src/test/java/um/edu/mt/cps2002/GameTest.java
@@ -12,22 +12,23 @@ import static org.junit.Assert.*;
 
 public class GameTest
 {
-    Game gameTest;
+
 
     @Before
     public void setUp()
     {
-        gameTest = new Game();
+        Game.map = new Map();
+
     }
     @Test
     public void testSetNumPlayers()
     {
-        Assert.assertEquals(false, gameTest.setNumPlayers(1));
-        Assert.assertEquals(true, gameTest.setNumPlayers(2));
-        Assert.assertEquals(true, gameTest.setNumPlayers(5));
-        Assert.assertEquals(true, gameTest.setNumPlayers(8));
-        Assert.assertEquals(8, gameTest.players.length);
-        Assert.assertEquals(false, gameTest.setNumPlayers(9));
+        Assert.assertEquals(false, Game.setNumPlayers(1));
+        Assert.assertEquals(true, Game.setNumPlayers(2));
+        Assert.assertEquals(true, Game.setNumPlayers(5));
+        Assert.assertEquals(true, Game.setNumPlayers(8));
+        Assert.assertEquals(8, Game.players.length);
+        Assert.assertEquals(false, Game.setNumPlayers(9));
 
     }
 
@@ -35,18 +36,18 @@ public class GameTest
     @Test
     public void testGenerateHTMLFiles()
     {
-        gameTest.setNumPlayers(2);
-        gameTest.players = new Player[2];
-        gameTest.players[0] = new Player();
-        gameTest.players[0].setPosition(new Position(0,0));
-        gameTest.players[1] = new Player();
-        gameTest.players[1].setPosition(new Position(5,5));
+        Game.setNumPlayers(2);
+        Game.players = new Player[2];
+        Game.players[0] = new Player(Game.map.tileColours);
+        Game.players[0].setPosition(new Position(0,0));
+        Game.players[1] = new Player(Game.map.tileColours);
+        Game.players[1].setPosition(new Position(5,5));
 
-        gameTest.map = new Map();
-        gameTest.map.setMapSize(6, 2);
+        Game.map = new Map();
+        Game.map.setMapSize(6, 2);
 
 
-        gameTest.players[0].mapKnowledge = new Colour[][]{
+        Game.players[0].mapKnowledge = new Colour[][]{
                 {Colour.GREEN, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY},
                 {Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY},
                 {Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY},
@@ -54,7 +55,7 @@ public class GameTest
                 {Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY},
                 {Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY},
         };
-        gameTest.players[1].mapKnowledge = new Colour[][]{
+        Game.players[1].mapKnowledge = new Colour[][]{
                 {Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY},
                 {Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY},
                 {Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY},
@@ -62,7 +63,8 @@ public class GameTest
                 {Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY},
                 {Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREY, Colour.GREEN},
         };
-        gameTest.generateHTMLFiles("./src/test/resources/um/edu/mt/cps2002/");
+        Game.generateHTMLFiles(Game.players[0],1);
+        Game.generateHTMLFiles(Game.players[1],2);
 
         try {
             //https://stackoverflow.com/questions/3891375/how-to-read-a-text-file-resource-into-java-unit-test
--- src/test/java/um/edu/mt/cps2002/PlayerTest.java
+++ src/test/java/um/edu/mt/cps2002/PlayerTest.java
@@ -8,23 +8,24 @@ import org.junit.Test;
 public class PlayerTest
 {
     Player playerTest;
+    Map map;
 
     @Before
     public void setUp()
-    {
-        playerTest = new Player();
+    {   map = new Map();
+        playerTest = new Player(map.tileColours);
     }
     @Test
     public void testMove()
     {
         playerTest.position = new Position(0,0);
-        playerTest.move(Direction.RIGHT);
+        playerTest.move(Direction.RIGHT,map);
         Assert.assertEquals(true, playerTest.position.equals(0,1));
-        playerTest.move(Direction.DOWN);
+        playerTest.move(Direction.DOWN,map);
         Assert.assertEquals(true, playerTest.position.equals(1,1));
-        playerTest.move(Direction.UP);
+        playerTest.move(Direction.UP,map);
         Assert.assertEquals(true, playerTest.position.equals(0,1));
-        playerTest.move(Direction.LEFT);
+        playerTest.move(Direction.LEFT,map);
         Assert.assertEquals(true, playerTest.position.equals(0,0));
     }
 
